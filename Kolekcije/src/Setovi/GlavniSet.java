19a2efc93b7abb03cea3ad4f4f4479c3ce79eee6
okkursevi8 <okkursevi8@UC5PC4>
2019-11-26 21:45:23 +0100

    Mape i Setovi

---
+++ Kolekcije/src/Setovi/GlavniSet.java
@@ -0,0 +1,63 @@
+package Setovi;
+
+import java.util.*;
+
+public class GlavniSet {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+			System.out.println("Unesite prvo ime:");
+			String ime1 = s.nextLine();
+			
+			System.out.println("Unesite drugo ime:");
+			String ime2 = s.nextLine();
+			
+			System.out.println("Unesite trece ime:");
+			String ime3 = s.nextLine();
+		
+		List<String> imena = new ArrayList<String>();
+			imena.add(ime1);
+			imena.add(ime2);
+			imena.add(ime3);
+		
+		System.out.println("Imena iz liste su: ");
+		
+		for (String nekoIme: imena) {
+			System.out.println(nekoIme);
+		}
+		
+		System.out.println("Lista preko iteratora");
+		
+		Iterator <String>listIterator = imena.iterator();
+		
+		while (listIterator.hasNext()) {
+			String ime = (String)listIterator.next();
+			System.out.println(ime);
+		}
+		
+		System.out.println("Ovde ide SET");
+		
+		Set<String> set = new HashSet<String>();
+			set.add(ime1);
+			set.add(ime2);
+			set.add(ime3);
+		
+		System.out.println("Imena iz seta su: ");
+		
+		for(String nekoIme: set) {
+			System.out.println(nekoIme);
+		}
+		
+		System.out.println("Set preko iteratora: ");
+		
+		Iterator <String> setIterator = set.iterator();
+		
+		while(setIterator.hasNext()) {
+			String ime = setIterator.next();
+			System.out.println(ime);
+		}
+		
+	}
+
+}
